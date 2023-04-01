45fc29c6c4289a8dd5be1527208de7446594f4da
Joseph Weller <joseph5062a@gmail.com>
2023-03-31 20:51:29 -0400

    headers for new blog

--- pages/blog/cocoa-puffs.tsx
+++ pages/blog/cocoa-puffs.tsx
@@ -18,16 +18,35 @@ const BlogPost: NextPage = () => {
         <div className='mx-auto pt-36'>
         <div className='text-white text-left mx-5 mb-24'>
             <div className='text-4xl font-bold'>
-                The World&apos;s Largest Cereal Bowl; an Ode to Absurdism
+                How to Make the World&apos;s Largest Bowl of Cereal
             </div>
             <div className='text-xl child:mt-5 mt-8'>
               <p>*record scratch* Yep, that&apos;s me. You&apos;re probably wondering how I got here.</p>
               <div className='flex justify-center'>
-                <Image width={700} height={466} src={"/img/blog/knee_deep.jpg"} alt="knee_deep" className='rounded-xl'/>
+                <div className='inline-block text-center text-xs'>
+                    <Image width={700} height={466} src={"/img/blog/knee_deep.jpg"} alt="knee_deep" className='rounded-xl'/>
+                    <p>Knee deep in a 9 foot tall dumpster full of Cocoa Puffs</p>
+                </div>
               </div>
-              <p>text</p>
-              <p className='text-2xl font-bold pt-3'>heading</p>
-              <p>text</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 1: Obtain 10,000 lbs of Cereal for Free</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 2: Store 9,000 lbs of Cereal for Free</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 3: Store 8,000 lbs of Cereal for Not Free</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 4: <p className='inline line-through'>Contemplate Existence</p> Make a Plan</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 5: Got Milk?</p>
+              <p>A</p>{/* Yes, 550 Gallons of Milk */}
+              <p className='text-2xl font-bold pt-3'>Step 6: Praise Capitalism</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 7: Showtime</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 8: Swimming in It</p>
+              <p>A</p>
+              <p className='text-2xl font-bold pt-3'>Step 9: Condemn Capitalism</p>
+              <p>A</p>
             </div>
           </div>
         </div>
