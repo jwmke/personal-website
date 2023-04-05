330a357f0b9b4675071f8d02aa660e801d7db5cd
Joseph Weller <joseph5062a@gmail.com>
2023-04-04 23:30:06 -0400

    more blog

--- pages/blog/cocoa-puffs.tsx
+++ pages/blog/cocoa-puffs.tsx
@@ -30,9 +30,10 @@ const BlogPost: NextPage = () => {
                 </div>
               </div>
               <p>To get there, we first need to rewind by 6 months from when this picture was taken, to when my roommate approached me, seemingly innocently, to show me a listing on one of the most problematic digital markets to gain traction since the Silk Road, Facebook Marketplace.</p> {/* https://en.wikipedia.org/wiki/Silk_Road_(marketplace) */}
-              <p className='text-2xl font-bold pt-3'>Step 1: Obtain 10,000 lbs of Cereal for Free</p>
-              <p>It all began with an anonymous General Mills truck driver, we&apos;ll call him Carl, whose objective for the week was to transport a trailer of 24 pallets of Cocoa Puffs from coast to coast.</p>
-              <p>As Carl reached the end of his journey he noticed a fatal mistake he had made at the start it. This error pertained to the tamper tag on his trailer, or rather, the lack thereof.</p>
+              <p className='text-3xl font-bold pt-3'>Step 0: Get Lucky</p>
+              <p className='italic'>&quot;Luck is when an opportunity comes along and you&apos;re prepared for it&quot;<br/>Denzel Washington</p>
+              <p>It all began with a General Mills truck driver, for the sake of anonimity, we&apos;ll call him Carl. Sometime in January of 2020, Carl was assigned an objective to transport a trailer of 24 pallets of Cocoa Puffs from one coast to the other.</p>
+              <p>As Carl reached the end of his journey he noticed a fatal mistake he had made at the start of it. This error pertained to the tamper tag on his trailer, or rather, the lack thereof.</p>
               <div className='flex justify-center'>
                 <div className='inline-block text-center text-xs'>
                   <Image width={468} height={244} src={"/img/blog/tag.jpg"} alt="tag" className='rounded-xl'/>
@@ -41,44 +42,59 @@ const BlogPost: NextPage = () => {
               </div>
               <p>For those who aren&apos;t as brushed up on your trucker terminology, a tamper tag is a glorified zip tie that&apos;s required by law to seal semi-trailers when they&apos;re transporting consumable goods. If the tag is removed in transit, or, in the case of Carl, forgotten to be put on in the first place, the goods are considered to be compromised, and must either be:</p>
               <ol>
-                <li>1. Incinerated</li>
-                <li>2. Buried in a landfill</li>
-                <li>3. Given away for free</li>
+                <li className='pb-2'>1. Incinerated</li>
+                <li className='pb-2'>2. Buried in a landfill</li>
+                <li className='pb-2'>3. Given away for free</li>
               </ol>
               <p>With Carl&apos;s villain origin story out of the way, we come back to my roommate who, with a slight grin on his face, is handing his phone to me with a Marketplace listing on-screen.</p>
-              <p className='font-bold'>FREE - 24 Pallets of Cocoa Puffs. First Come, First Served.</p>
-              <p>I took a quick glance at the post, &quot;haha, pretty interesting dude&quot;, and as I&apos;m handing his phone back I notice a glisten in his eye as begins to speak.</p>
-              <p>Being honest, I don&apos;t remember a majority of the conversation that followed, likely because my brain repressed it as a response to the traumatic stress of the ensuing clown carnival ride of experiences that followed. However, in essence, my roommate wanted to do something big. Bigger than anything he, or anyone he knew, had done before. And he viewed these 24 pallets of cereal as being the gateway to doing that big thing, agnostic of what that big thing might be.</p>
+              <p className='text-2xl font-bold'>FREE - 24 Pallets of Cocoa Puffs. First Come, First Served.</p>
+              <p>I took a quick glance at the post, &quot;haha, pretty interesting dude&quot;, and as I&apos;m handing his phone back I notice a glisten in his eye as he begins to speak.</p>
+              <p>Honestly, I don&apos;t remember a majority of the conversation that followed, likely because my brain repressed it as a response to the traumatic stress of the ensuing clown carnival ride of experiences that followed. However, in essence, my roommate wanted to do something big. Bigger than anything he, or anyone he knew, had done before. And he viewed these 24 pallets of cereal as being the gateway to doing that big thing, agnostic of what that big thing might be.</p>
               <div className='flex justify-center'>
                 <div className='inline-block text-center text-xs'>
                   <Image width={468} height={244} src={"/img/blog/pallet.jpg"} alt="pallet" className='rounded-xl'/>
-                  <p>Pallet of Cocoa Puffs, skid-steer for scale</p>
+                  <p>Pallet containing a ~600 lb bag of Cocoa Puffs, skid-steer for scale</p>
                 </div>
               </div>
               <p>While my head was overflowing with questions, he abruptly asked if I would help him &quot;get the cereal.&quot; I was reluctant, but since our winter break was less than a week away, and I didn&apos;t have any other plans, I figured that helping out with this, whatever <p className='inline italic'>this</p> was going to be, would likely be more entertaining than immediately heading home, so I agreed.</p>
-              <p>He organized a day to meet up with the man who was in possession of the pallets and reached out to around 10 other friends, who turned out to be just as bored and curious as me, to see if they&apos;d be willing to help.</p>
+              <p>He organized a day to meet up with the man who was in possession of the pallets and reached out to around 10 other friends, who turned out to be just as bored yet curious as me, to see if they&apos;d be willing to lend a hand.</p>
               <p>Then, with an army amassed, the ringleader and his troops marched into war.</p>
-              <p className='text-2xl font-bold pt-3'>Step 2: Store 9,000 lbs of Cereal for Free</p>
-              <p>A</p>
+              <p className='text-3xl font-bold pt-3'>Step 1: Obtain 10,000 lbs of Cereal for Free</p>
+              <p>The plan was simple. Two phases. In and out.</p>
+              <p className='text-2xl font-bold'>Phase 1, Procuring</p>
+              <ol>
+                <li className='pb-2'>1. Rent the largest moving truck we could legally drive and attach a trailer to it for good measure</li>
+                <li className='pb-2'>2. Pull up to the cargo yard where the semi-trailer containing the Cocoa Puffs was located</li>
+                <li>3. Fill every square inch of the moving truck with cereal, first with pallets, then with garbage bags full of cereal to fill in all nooks and crannies <p className='italic inline'>(shoutout to Phil and his friends from the cargo yard for letting us use their heavy machines and for offering us some of their weed (we graciously declined))</p></li>
+              </ol>
+              <p>Once dusk was upon us, we would pack up and drive to a self-storage facility 40 minutes from the cargo yard. The second phase of the plan would be then be initiated.</p>
               <div className='flex justify-center'>
                 <div className='inline-block text-center text-xs'>
                   <Image width={468} height={244} src={"/img/blog/uhaul.jpg"} alt="uhaul" className='rounded-xl'/>
                   <p>The CocoaMobile</p>
                 </div>
               </div>
-              <p className='text-2xl font-bold pt-3'>Step 3: Store 8,000 lbs of Cereal for Not Free</p>
+              <p className='text-2xl font-bold'>Phase 2, Hoarding</p>
+              <ol>
+                <li className='pb-2'>1. Redeem two <p className='inline font-bold'>first-month free</p> deals for the self-storage place&apos;s largest lockers. This would be done under the name of the two of us who drew the shortest straws</li>
+                <li className='pb-2'>2. Discreetly unload ten thousand pounds of cereal into the lockers, this time without heavy machines</li>
+                <li className='pb-2'>3. Wait 29 days before drawing straws again to determine who would use the same deal to sign up for two new lockers, and then proceed to migrate all of the cereal from locker to locker</li>
+                <li>4. Rinse and repeat the previous step until we determine what we&apos;re going to do with ten thousand pounds of Cocoa Puffs</li>
+              </ol>
+              <p>All in all, the first half of the day went according to plan, however, once we approached the second phase, we began to encounter just a few slight minor tiny hiccups.</p>
+              <p className='text-3xl font-bold pt-3'>Step 2: Survive</p>
               <p>A</p>
-              <p className='text-2xl font-bold pt-3'>Step 4: <p className='inline line-through'>Contemplate Existence</p> Make a Plan</p>
+              <p className='text-3xl font-bold pt-3'>Step 3: <p className='inline line-through'>Contemplate Existence</p> Make a Plan</p>
               <p>A</p>
-              <p className='text-2xl font-bold pt-3'>Step 5: Got Milk?</p>
+              <p className='text-3xl font-bold pt-3'>Step 4: Got Milk?</p>
               <p>A</p>{/* Yes, 550 Gallons of Milk */}
-              <p className='text-2xl font-bold pt-3'>Step 6: Praise Capitalism</p>
+              <p className='text-3xl font-bold pt-3'>Step 5: Praise Capitalism</p>
               <p>A</p>
-              <p className='text-2xl font-bold pt-3'>Step 7: Showtime</p>
+              <p className='text-3xl font-bold pt-3'>Step 6: Showtime</p>
               <p>A</p>
-              <p className='text-2xl font-bold pt-3'>Step 8: Swimming in It</p>
+              <p className='text-3xl font-bold pt-3'>Step 7: Swimming in It</p>
               <p>A</p>
-              <p className='text-2xl font-bold pt-3'>Step 9: Condemn Capitalism</p>
+              <p className='text-3xl font-bold pt-3'>Step 8: Condemn Capitalism</p>
               <p>A</p>
             </div>
           </div>
--- pages/blog/new-beginning.tsx
+++ pages/blog/new-beginning.tsx
@@ -27,14 +27,14 @@ const BlogPost: NextPage = () => {
               </div>
               <p>Up until then, graduation was the light at the end of the tunnel that my peers and I had been wandering toward for the entirety of our emerging adulthood. This wandering was more or less aimless as I naively thought that all of the efforts I was contributing were in the name of escaping some sort of metaphoric prison that was infringing on my sense of agency.</p>
               <p>In reality, that prison was a sanctuary.</p>
-              <p className='text-2xl font-bold pt-3'>The Price We Pay for Love</p>
+              <p className='text-3xl font-bold pt-3'>The Price We Pay for Love</p>
               <p className='italic'>&quot;How lucky I am to have something that makes saying goodbye so hard?&quot;<br/>Winnie-the-Pooh</p>
               <p>Narrowing my attention to the short-term future, I could only focus on what was going to be lost upon graduating. I progressively became more aware of everything that I had been taking for granted; as I did, my melodramatic side felt like the world around me was collapsing in on itself.</p>
               <p>I first began to fear the loss of structure that university provided. In one way or another, every aspect of university was methodically planned out. From the socializing that&apos;s built into the framework of education, to the foundational routines of learning and activities that were omnipresent, each component of university was organized such that I could continuously improve myself, as well as enjoy myself, all while essentially living on autopilot.</p>
               <p>After more reflection, I realized the loss of community that would occur. Never before in my life had I made connections as deep as the ones that I built up with my friends and professors. While I knew it would still be possible to maintain these connections with my closest friends post-graduation, the idea of no longer being able to easily communicate and banter with my peers made me feel as if I was leaving a significant part of my life behind.</p>
               <p>Lastly, I became aware of the loss of identity that I would experience. There had never been a time period in my sentient past where I wasn&apos;t a student. The idea of this student fragment of my identity being torn away sent me into a state of confusion and disconnectedness. In addition to this, I became painfully aware of my own mortality, which I had been blissfully ignoring for the previous 22 years of my life.</p>
               <p>When the day finally came, and I walked across that stage, nothing had ever felt more anticlimactic. I had seemingly spent my entire life working towards this exact moment, yet I was nearly entirely void of emotion. As I walked back to my seat and sat down I thought to myself, &quot;what now?&quot;</p>
-              <p className='text-2xl font-bold pt-3'>Moving Forward</p>
+              <p className='text-3xl font-bold pt-3'>Moving Forward</p>
               <div className='flex justify-center'>
                 <Image width={468} height={244} src={"/img/blog/copium.jpg"} alt="copium" className='rounded-xl'/>
               </div>
@@ -44,7 +44,7 @@ const BlogPost: NextPage = () => {
               <p>But how does one craft their own definition of success? In my opinion, this is one of the most complex, multivariable questions that can be asked. It is synonymous with determining the meaning of life, and nearly every indivudual on the planet will have a varying answer. Nonetheless, I&apos;ll attempt to solicit some guidance on how you can begin to approach the question.</p>
               <p>First, you must understand that your answer must be malleable and will therefore change over the course of your life, therefore, there&apos;s no purpose in stressing about creating the perfect answer now. When designing your answer, you&apos;ll want to consider both your values and your aspirations in life. These are the elements in life that are truly the most important to you and the corresponding goals that meaningfully align with them.</p>
               <p>Not only is it necessary to maintain a flexible mindset and be receptive to feedback once you determine your answer, but you also mustn&apos;t be too strict on yourself in the pursuit of this success. After all, the best of life is experienced on the detours and adventures along the way, not at the destination.</p>
-              <p className='text-2xl font-bold pt-3'>It&apos;s the Journey</p>
+              <p className='text-3xl font-bold pt-3'>It&apos;s the Journey</p>
               <div className='flex justify-center'>
                 <Image width={468} height={244} src={"/img/blog/obi.gif"} alt="obi" className='rounded-xl' unoptimized={true}/>
               </div>
