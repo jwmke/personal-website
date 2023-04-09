8a719b387f74bbdf6439abd21fd2b9b2e174a568
Joseph Weller <joseph5062a@gmail.com>
2023-04-08 20:42:14 -0400

    survive blog update

--- pages/blog/cocoa-puffs.tsx
+++ pages/blog/cocoa-puffs.tsx
@@ -6,7 +6,6 @@ import Navbar from '../../components/Navbar';
 
 const BlogPost: NextPage = () => {
     return (
-      
       <div className='font-lato w-full flex bg-gradient-to-b from-teal to-navy select-none'>
       <Head>
           <title>Joe Weller</title>
@@ -48,7 +47,7 @@ const BlogPost: NextPage = () => {
               </ol>
               <p>With Carl&apos;s villain origin story out of the way, we come back to my roommate who, with a slight grin on his face, is handing his phone to me with a Marketplace listing on-screen.</p>
               <p className='text-2xl font-bold'>FREE - 24 Pallets of Cocoa Puffs. First Come, First Served.</p>
-              <p>I took a quick glance at the post, &quot;haha, pretty interesting dude&quot;, and as I&apos;m handing his phone back I notice a glisten in his eye as he begins to speak.</p>
+              <p>I took a quick glance at the post, &quot;haha, pretty interesting dude&quot;, and as I&apos;m handing his phone back I notice a glisten in his eye as he began to speak.</p>
               <p>Honestly, I don&apos;t remember a majority of the conversation that followed, likely because my brain repressed it as a response to the traumatic stress of the ensuing clown carnival ride of experiences that followed. However, in essence, my roommate wanted to do something big. Bigger than anything he, or anyone he knew, had done before. And he viewed these 24 pallets of cereal as being the gateway to doing that big thing, agnostic of what that big thing might be.</p>
               <div className='flex justify-center'>
                 <div className='inline-block text-center text-xs'>
@@ -81,9 +80,38 @@ const BlogPost: NextPage = () => {
                 <li className='pb-2'>3. Wait 29 days before drawing straws again to determine who would use the same deal to sign up for two new lockers, and then proceed to migrate all of the cereal from locker to locker</li>
                 <li>4. Rinse and repeat the previous step until we determine what we&apos;re going to do with ten thousand pounds of Cocoa Puffs</li>
               </ol>
-              <p>All in all, the first half of the day went according to plan, however, once we approached the second phase, we began to encounter just a few slight minor tiny hiccups.</p>
+              <p>All in all, the first half of the day went according to plan, however, once we approached the second phase, we began to encounter just a few <p className='inline text-lg'>slight</p> <p className='inline text-base'>minor</p> <p className='inline text-sm'>tiny</p> <p className='inline text-xs'>hiccups.</p></p>
               <p className='text-3xl font-bold pt-3'>Step 2: Survive</p>
-              <p>A</p>
+              <p>At around 5:00 PM, as the sun was setting, we departed from the cargo yard, beginning our trek to the self-storage facility.</p>
+              <p>Leading the convoy was a minivan, (also filled to the brim with cereal garbage bags), behind it, the CocoaMobile in all its glory, and the trailing escort vehicle was an SUV carrying the remainder of the infantry.</p>
+              <p>The CocoaMobile may or may not have been just a pound or two <p className='inline line-through'>thousand</p> over its suggested haul limit, so the SUV was strategically following it at the tail end of the convoy, with the passengers of both vehicles on speed dial with each other in case any catastrophes were to arise.</p>
+              <p>With the exception of the fact that the CocoaMobile max speed was 10 miles under the highway speed limit, the voyage was going smoothly until around 20 minutes in.</p>
+              <p>Out of nowhere, an NPC truck attempted to cut off CocoaMobile, causing its driver to slam on the brakes. This caused the trailer to force itself off the hitch, meaning the trailer was only attached by the safety chains as the front of it was grinding itself into nothingness against the road.</p>
+              <div className='flex justify-center'>
+                <div className='inline-block text-center text-xs'>
+                  <Image width={468} height={244} src={"/img/blog/cord.jpg"} alt="cord" className='rounded-xl'/>
+                  <p>Break-light cord that got guillotined by the trailer&apos;s safety chains, kept as a souvenir</p>
+                </div>
+              </div>
+              <p>It was easy to notice this was happening from the SUV&apos;s point of view, not only because of the light show of sparks that suddenly occurred but also because the trailer began to start violently fishtailing back and forth.</p>
+              <p>The passenger of the SUV nearly immediately called the CocoaMobile and politely screamed at the driver what was happening, and that he needed to speed up, (to stop the fishtailing), then slow down and pull over.</p>
+              <p>Luckily the driver was able to get to the side of the highway safely, but if that call would have been made even 10 seconds later, this post likely would have had a different title.</p>
+              <p className='text-2xl font-bold'>How to Cause a Three-Hour Delay on Interstate-94 Using Only 5 Tons of Cereal</p>
+              <p>After we reattached the trailer and mentally confirmed that there was now only a {"<5%"} chance that we&apos;d be receiving our deposit back. Our journey resumed, and somehow, we eventually made it to the self-storage facility.</p>
+              <p>I&apos;ll just give the SparkNotes about how the second phase unfolded so that we can get to the meat and potatoes of the guide, i.e. the cereal bowl.</p>
+              <ul>
+                <li className='pb-2'>• Having no heavy machinery meant that we had to simply push and drop the pallets off the back of the CocoaMoble, and pray they didn&apos;t explode in the process (most didn&apos;t)</li>
+                <li className='pb-2'>• The rented lockers were randomly selected to be on the second floor of the facility, meaning we not only had to bring the cereal to them by hand, but we also had to fit everything in an elevator with an opening barely wider than the width of the pallets</li>
+                <li className='pb-2'>• The garbage bags and pallets were constantly leaking and tearing, leaving piles of Cocoa Puffs everywhere in our path (parking lot, corridors, elevator shaft, etc.) as we were transporting them to the lockers. This resulted in a total loss of around 1000 pounds of puffs</li>
+                <li>• Our unlimited free storage hack was discovered the second time we were transferring the cereal to different lockers, so we had to move all of the cereal to a new self-storage facility, resulting in around another 1000 lost pounds of puffs</li>
+              </ul>
+              <div className='flex justify-center'>
+                <div className='inline-block text-center text-xs'>
+                  <Image width={468} height={244} src={"/img/blog/before-after.gif"} alt="before-after" className='rounded-xl'/>
+                  <p>Proof we cleaned up because we&apos;re not a-holes</p>
+                </div>
+              </div>
+              <p>Now that we had <p className='inline line-through'>10K</p> <p className='inline line-through'>9K</p> 8K pounds of cereal in our possession, we sat down and asked ourselves, why?</p>
               <p className='text-3xl font-bold pt-3'>Step 3: <p className='inline line-through'>Contemplate Existence</p> Make a Plan</p>
               <p>A</p>
               <p className='text-3xl font-bold pt-3'>Step 4: Got Milk?</p>
