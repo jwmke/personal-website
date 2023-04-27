36c30dd9b826b4914c54553655615fbecfe975e8
Joseph Weller <joseph5062a@gmail.com>
2023-04-26 20:59:16 -0400

    draft dir

--- pages/blog/draft/electric.tsx
+++ pages/blog/draft/electric.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-import Navbar from '../../components/Navbar';
+import Navbar from '../../../components/Navbar';
 
 const BlogPost: NextPage = () => {
     return (
@@ -9,7 +9,7 @@ const BlogPost: NextPage = () => {
       <div className='font-lato w-full flex bg-gradient-to-b from-teal to-navy select-none'>
       <Head>
           <title>Joe Weller</title>
-          <meta name="description" content="" /> {/* TODO: add content */}
+          <meta name="description" content="Gotta go fast." />
           <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
       <Navbar/>
@@ -20,9 +20,14 @@ const BlogPost: NextPage = () => {
                 Title
             </div>
             <div className='text-xl child:mt-5 mt-8'>
-              <p>text</p>
+              <p>https://shop.niu.com/products/niu-kqi3-max-electric-kick-scooter-for-adults</p>
+              <p>https://fluidfreeride.com/products/fluid-mosquito?ref=18</p>
+              <p>https://www.euco.us/products/inmotion-v10-electric-unicycle</p>
+              <p>https://onewheel.com/products/onewheel-pint-x</p>
+              <p>https://www.backfireboards.com/products/backfire-zealot-belt-drive-electric-skateboard</p>
+              <p>https://www.exwayboard.com/products/exway-atlas-carbon-2wd</p>
               <p className='text-2xl font-bold pt-3'>heading</p>
-              <p>text</p>
+              <p>Portable, no e-bikes. Practical, no hoverboards. </p>
             </div>
           </div>
         </div>
