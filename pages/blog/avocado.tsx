8e3adb25c66fabcfd5f55724845eccc2688fb8ab
Joseph Weller <joseph5062a@gmail.com>
2023-04-29 19:30:14 -0400

    recipe updates

--- pages/blog/avocado.tsx
+++ pages/blog/avocado.tsx
@@ -23,13 +23,14 @@ const BlogPost: NextPage = () => {
               <p>The only snack in my diet that&apos;s both healthy and somehow tastes good. Here&apos;s how to quickly make it with minimal cleanup involved.</p>
               <p className='text-2xl font-bold pt-3'>Ingredients</p>
               <p>2 Slices of Bread (Sourdough or Whole Grain)</p>
-              <p>1 Ripe Avocado</p>
+              <p>1 Hass Avocado</p>
               <p>.5 tsp, Extra Virgin Olive Oil</p>
               <p>.25 tsp, Sea Salt</p>
               <p>.25 tsp, White Pepper</p>
               <p>.25 tsp, <a href='https://www.traderjoes.com/home/products/pdp/ground-fermented-black-garlic-067160' target="_blank" rel="noreferrer" className='hover:text-teal underline hover:cursor-nesw-resize'>Ground Fermented Black Garlic</a> (or Garlic Powder)</p>
               <p>.25 tsp, Red Chili Flakes </p>
               <p>.5 tsp, Sesame Seeds</p>
+              <p>1/4 cup, <a href='https://www.culinaryhill.com/how-to-toast-walnuts/' target="_blank" rel="noreferrer" className='hover:text-teal underline hover:cursor-nesw-resize'>Toasted Walnuts</a> (optional)</p>
               <p className='text-2xl font-bold pt-3'>Preparation Tips</p>
               <p>While toasting the bread, use a large knife to slice the Avocado into quarters, vertically.</p>
               <p>For each quarter, peel off the skin, slice it vertically 2-3 times, and then set the slices aside.</p>
