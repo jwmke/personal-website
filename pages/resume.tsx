d3b08f64971ef2139d179cb1b9193091341b5ed7
Joseph Weller <joseph5062a@gmail.com>
2024-03-15 21:02:42 -0400

    rm page

---
+++ pages/resume.tsx
@@ -0,0 +1,18 @@
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+export default function Resume() {
+    return (
+        <>
+            <div className='bg-white'>
+                <Document file="/Weller_Joseph_Resume_2024.pdf">
+                    <Page pageNumber={1} renderTextLayer={false} scale={1.75} canvasBackground='#edf5e1'/>
+                </Document>
+            </div>
+            <div className='flex justify-center'>
+                {":)"}
+            </div>
+        </>
+    )
+}
