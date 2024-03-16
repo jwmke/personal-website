2c90df0dce53703507e0241ab3adf08cccf76b82
Joseph Weller <joseph5062a@gmail.com>
2024-03-16 18:26:33 -0400

    mobile scaling

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,13 +1,16 @@
 import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 export default function Resume() {
+    const { width } = useWindowDimensions();
+    const scale = width < 768 ? .7 : 1.75;
     return (
         <>
             <div className='bg-white'>
                 <Document file="/Weller_Joseph_Resume_2024.pdf">
-                    <Page pageNumber={1} renderTextLayer={false} scale={1.75} canvasBackground='#edf5e1'/>
+                    <Page pageNumber={1} renderTextLayer={false} scale={scale} canvasBackground='#edf5e1'/>
                 </Document>
             </div>
         </>
